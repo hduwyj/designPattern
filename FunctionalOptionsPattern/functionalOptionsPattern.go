31d75835570ad37d72d9d29f60cf5ae896cfe21e
wangyujiang <hdu_wangyujiang@163.com>
2018-10-23 18:45:24 +0800

    选项模式。适用情况： 1.参数确实比较复杂，影响调用方使用 2.参数确实有比较清晰明确的默认值 3.为参数的后续拓展考虑

---
+++ FunctionalOptionsPattern/functionalOptionsPattern.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type identity struct {
+	Name         string
+	IdCardNumber string
+	Age          int
+	Email        string
+}
+
+//可选参数列表
+type IdentityOptions struct {
+	Age   int
+	Email string
+}
+
+type IdentityOption func(options *IdentityOptions)
+
+func WithAge(age int) IdentityOption {
+	return func(options *IdentityOptions) {
+		options.Age = age
+	}
+}
+
+func WithEmail(email string) IdentityOption {
+	return func(options *IdentityOptions) {
+		options.Email = email
+	}
+}
+
+var defaultIdentity = IdentityOptions{18, "hdu_wangyujiang@163.com"}
+
+func NewIdentity(name string, idCard string, opts ...IdentityOption) *identity {
+	id := defaultIdentity
+	for _, o := range opts {
+		o(&id)
+	}
+	return &identity{name, idCard, id.Age, id.Email}
+}
+func main() {
+	x := NewIdentity("WANGYUJIANG", "340", WithAge(20), WithEmail("sd"))
+	fmt.Println(x)
+
+}
